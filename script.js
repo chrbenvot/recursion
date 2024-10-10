1d9663f1764948f30a6a5040f2b022ad2faf8334
chrbenvot <hammamhiri@gmail.com>
2024-10-10 17:53:34 +0100

    Do project

---
+++ script.js
@@ -0,0 +1,72 @@
+function fibs(n){
+    let arr=[0,1];
+    for (let i=2;i<n;i++){
+        arr.push(arr[i-2]+arr[i-1])
+    };
+    return arr
+}
+function fibsRec(n){
+    if (n<=2){
+        return [0,1]
+    }
+    else{
+        let aux=fibsRec(n-1);
+        aux.push(aux[n-3]+aux[n-2])
+        return aux
+    }
+}
+
+function merge(arr, left, mid, right) {
+    const n1 = mid - left + 1;
+    const n2 = right - mid;
+
+    // Create temp arrays
+    const L = new Array(n1);
+    const R = new Array(n2);
+
+    // Copy data to temp arrays L[] and R[]
+    for (let i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (let j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    let i = 0, j = 0;
+    let k = left;
+
+    // Merge the temp arrays back into arr[left..right]
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+function mergeSort(arr, left, right) {
+    if (left >= right)
+        return;
+
+    const mid = Math.floor(left + (right - left) / 2);
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
